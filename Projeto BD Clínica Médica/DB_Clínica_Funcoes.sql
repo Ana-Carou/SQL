d859c6a6335a5aec94122dc1bba436b41524bb96
Ana-Carou <148507974+Ana-Carou@users.noreply.github.com>
2023-12-15 23:27:15 -0300

    Update DB_Clínica_Funcoes.sql

--- Projeto BD Clínica Médica/DB_Clínica_Funcoes.sql
+++ Projeto BD Clínica Médica/DB_Clínica_Funcoes.sql
@@ -1,5 +1,6 @@
--- - Script de criação de funções
- 
+-- - Script de criação de funções 
+
+-- - Cálculo da idade dos pacientes - -- 
 DELIMITER //
 CREATE FUNCTION fn_idade_paciente(data_nascimento DATE) 
 RETURNS INT
@@ -14,3 +15,23 @@ DELIMITER ;
 
 SELECT concat(primeiro_nome, ' ', ultimo_nome) as paciente, fn_idade_paciente(data_nascimento) as idade 
 FROM paciente;
+
+
+-- - Contar número de pacientes por médico
+
+DELIMITER //
+CREATE FUNCTION PacientePorMedico(medico INT) 
+RETURNS INT
+READS SQL DATA
+BEGIN
+    DECLARE numPacientes INT;
+    SELECT COUNT(*) INTO numPacientes
+    FROM paciente
+    WHERE id_medico = medico;
+    RETURN numPacientes;
+END;
+//
+DELIMITER ;
+
+select PacientePorMedico(), primeiro_nome
+from medico;
